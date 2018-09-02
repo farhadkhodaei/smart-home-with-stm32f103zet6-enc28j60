3683b7c9a4b66d8df261ae7845968b9617edf1d2
Farhad Khodaei <39498931+ggfarhad@users.noreply.github.com>
2018-09-03 00:38:42 +0430

    Add files via upload

--- f103/Src/main.c
+++ f103/Src/main.c
@@ -112,9 +112,9 @@ int main(void)
 
   /* USER CODE BEGIN 2 */
     delay_us(50);
-    //GPIOA->ODR &= ~(1<<10);
+    GPIOC->ODR &= ~(1<<13);
 	  delay_us(50);
-    //GPIOA->ODR |= (1<<10);
+    GPIOC->ODR |= (1<<13);
 	  delay_us(50);
 		GPIOF->ODR |= 15<<6;
     ENC28J60_Init(mymac);
@@ -254,14 +254,24 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOF, GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PF6 PF7 PF8 PF9 */
   GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
